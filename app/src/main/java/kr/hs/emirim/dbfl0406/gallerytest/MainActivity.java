03a6dc1b57b230514af944c15e7738fdd7ab0376
Chaeyun Yang <ycy6685@naver.com>
2016-10-25 19:41:51 +0900

    #2 Gallery Adapter

--- app/src/main/java/kr/hs/emirim/dbfl0406/gallerytest/MainActivity.java
+++ app/src/main/java/kr/hs/emirim/dbfl0406/gallerytest/MainActivity.java
@@ -1,13 +1,63 @@
 package kr.hs.emirim.dbfl0406.gallerytest;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Context;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.Gallery;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
+
+    Gallery gallery;
+    ImageView imgMain;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        gallery=(Gallery)findViewById(R.id.gallery_1);
+        imgMain=(ImageView)findViewById(R.id.imgv_main);
+        MyGalleryAdapter adapter=new MyGalleryAdapter(this);
+        gallery.setAdapter(adapter);
+    }
+
+    public class MyGalleryAdapter extends BaseAdapter{
+
+        Context context;
+        int[] ImageIDs={R.drawable.img01,R.drawable.img02,R.drawable.img03,R.drawable.img04,
+                R.drawable.img05,R.drawable.img06,R.drawable.img07,R.drawable.img08,
+                R.drawable.img09,R.drawable.img10};
+
+        public MyGalleryAdapter(Context context){
+            this.context=context;
+        }
+
+        @Override
+        public int getCount() {
+            return ImageIDs.length;
+        }
+
+        @Override
+        public Object getItem(int position) {
+            return null;
+        }
+
+        @Override
+        public long getItemId(int position) {
+            return 0;
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            ImageView imgV=new ImageView(context);
+            imgV.setLayoutParams(new Gallery.LayoutParams(150,150));
+            imgV.setScaleType(ImageView.ScaleType.FIT_CENTER);
+            imgV.setPadding(5,5,5,5);
+            imgV.setImageResource(ImageIDs[position]);
+            return imgV;
+        }
     }
 }
